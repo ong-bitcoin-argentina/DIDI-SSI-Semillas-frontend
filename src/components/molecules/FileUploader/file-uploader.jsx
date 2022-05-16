95fb21ccf34402de8fda03fd7a50b6db397e7a61
Fabrizio Pouget <fpouget@folderit.net>
2022-05-16 15:23:33 -0300

    Cambio de Endpoint para la Generacion de ReportesPDF

--- src/components/molecules/FileUploader/file-uploader.jsx
+++ src/components/molecules/FileUploader/file-uploader.jsx
@@ -15,9 +15,15 @@ import './_style.scss';
 const { Panel } = Collapse;
 const { Dragger } = Upload;
 
-const { uploadFile, validateSancorFile, uploadSancorFile } = apiCalls();
+const { uploadFile, uploadPdfFile ,validateSancorFile, uploadSancorFile } = apiCalls();
 
-const FileUploader = ({ buttonText, source, onChangeSource, onUploaded, onSuccessRequest }) => {
+
+// onSuccessRequest={onUploaded}
+// source={{ name: 'socioeconomica' }}
+// buttonText="Generar Reportes"
+// createCredentials={false}
+// pdfValidation={true}
+const FileUploader = ({ buttonText, source, onChangeSource, onUploaded, onSuccessRequest,pdfValidation }) => {
   const [file, setFile] = useState(null);
   const [showContainer, setShowContainer] = useState(false);
   const [uploadResponse, setUploadResponse] = useState(null);
@@ -30,15 +36,27 @@ const FileUploader = ({ buttonText, source, onChangeSource, onUploaded, onSucces
       const formData = new FormData();
       formData.set('file', file);
       formData.set('createCredentials', createCredentials);
-      const uploadAction = source.name === 'sancor' ? uploadSancorFile : uploadFile;
-      const response = await uploadAction(formData);
-      if (onSuccessRequest && response.data.downloadableFileName) {
+      let uploadAction;
+      let response;
+      if(pdfValidation){
+        uploadAction = source.name === 'sancor' ? uploadSancorFile : uploadPdfFile;
+        response = await uploadAction(formData);
+
+        // if (onSuccessRequest && response.data.downloadableFileName) {
         onSuccessRequest(response);
+        // }
       } else {
-        !createCredentials && setUploadResponse(response.data);
-        createCredentials && onUploaded(response);
+        uploadAction = source.name === 'sancor' ? uploadSancorFile : uploadFile;
+        response = await uploadAction(formData);
+        if (onSuccessRequest && response.data.downloadableFileName) {
+          onSuccessRequest(response);
+        } else {
+          !createCredentials && setUploadResponse(response.data);
+          createCredentials && onUploaded(response);
+        }
       }
       setUploading(false);
+
     } catch (error) {
       showErrorMessage(processedErrorMessage(error), processError(error));
       setUploadResponse(null);
@@ -51,22 +69,22 @@ const FileUploader = ({ buttonText, source, onChangeSource, onUploaded, onSucces
       title: '¿Desea continuar?',
       icon: <ExclamationCircleOutlined />,
       content: (
-        <div>
-          <p>
-            Para reemplazar las credenciales de identidad duplicadas, primero debés revocar
-            manualmente las anteriores.
-          </p>
-          <p>
-            En caso de continuar,{' '}
-            <strong>
-              los cambios en las Credenciales de Identidad duplicadas serán descartados
-            </strong>
-            .
-          </p>
-        </div>
+          <div>
+            <p>
+              Para reemplazar las credenciales de identidad duplicadas, primero debés revocar
+              manualmente las anteriores.
+            </p>
+            <p>
+              En caso de continuar,{' '}
+              <strong>
+                los cambios en las Credenciales de Identidad duplicadas serán descartados
+              </strong>
+              .
+            </p>
+          </div>
       ),
       onOk() {
-        handleUpload(true);
+        handleUpload(true, true);
       }
     });
   };
@@ -106,8 +124,8 @@ const FileUploader = ({ buttonText, source, onChangeSource, onUploaded, onSucces
 
   const isDuplicatedIdentity = err => {
     return (
-      err.errorType === DUPLICATED_CREDENTIAL &&
-      err.category?.includes(CREDENTIAL_CATEGORIES.identity)
+        err.errorType === DUPLICATED_CREDENTIAL &&
+        err.category?.includes(CREDENTIAL_CATEGORIES.identity)
     );
   };
 
@@ -124,12 +142,12 @@ const FileUploader = ({ buttonText, source, onChangeSource, onUploaded, onSucces
     if (!hasResponseErrors || hasOnlyDuplicatedIdentityCredentials()) {
       action = !hasResponseErrors ? handleUpload : handleUploadSkipping;
       return (
-        <ButtonPrimary
-          text="Crear credenciales"
-          theme="ThemePrimary"
-          onClick={() => action(true)}
-          disabled={!showContainer}
-        />
+          <ButtonPrimary
+              text="Crear credenciales"
+              theme="ThemePrimary"
+              onClick={() => action(true)}
+              disabled={!showContainer}
+          />
       );
     }
   };
@@ -142,93 +160,93 @@ const FileUploader = ({ buttonText, source, onChangeSource, onUploaded, onSucces
 
   const renderValidation = () => {
     return (
-      <>
-        <Alert
-          message="Proceso de validación ejecutado correctamente."
-          type="info"
-          closable
-          showIcon
-          className="my-2"
-        />
-        <div>
-          {validation.totalErrorsRows === 0 ? (
-            <Alert message="El archivo no tiene errores" type="success" closable showIcon />
-          ) : (
-              <>
-                <Alert
-                  message={`Se encontraron: ${validation.totalErrorsRows} errores.`}
-                  type="error"
-                  showIcon
-                  className="my-2"
-                />
-                <Collapse>
-                  <Panel header="Detalle errores" key="1">
-                    {validation.errorRows.map(renderErrorItem)}
-                  </Panel>
-                </Collapse>
-              </>
+        <>
+          <Alert
+              message="Proceso de validación ejecutado correctamente."
+              type="info"
+              closable
+              showIcon
+              className="my-2"
+          />
+          <div>
+            {validation.totalErrorsRows === 0 ? (
+                <Alert message="El archivo no tiene errores" type="success" closable showIcon />
+            ) : (
+                <>
+                  <Alert
+                      message={`Se encontraron: ${validation.totalErrorsRows} errores.`}
+                      type="error"
+                      showIcon
+                      className="my-2"
+                  />
+                  <Collapse>
+                    <Panel header="Detalle errores" key="1">
+                      {validation.errorRows.map(renderErrorItem)}
+                    </Panel>
+                  </Collapse>
+                </>
             )}
-        </div>
-      </>
+          </div>
+        </>
     );
   };
 
   return (
-    <div className="FileUploader">
-      <div className="mainSectionReport">
-        <div className="UploadReport">
-          <div className="preview">
-            <Dragger {...props} onChange={onFileChanged} className={showContainer ? 'hidden' : ''}>
-              <img src="/img/upload-file.svg" alt="" />
-              <p className="ant-upload-text">
-                Haga clic o arrastre un archivo a este área para cargar
-              </p>
-              <p className="ant-upload-hint">Formatos aceptados: .xlsx</p>
-            </Dragger>
+      <div className="FileUploader">
+        <div className="mainSectionReport">
+          <div className="UploadReport">
+            <div className="preview">
+              <Dragger {...props} onChange={onFileChanged} className={showContainer ? 'hidden' : ''}>
+                <img src="/img/upload-file.svg" alt="" />
+                <p className="ant-upload-text">
+                  Haga clic o arrastre un archivo a este área para cargar
+                </p>
+                <p className="ant-upload-hint">Formatos aceptados: .xlsx</p>
+              </Dragger>
 
-            <div className="title">
-              {
-                showContainer &&
-                <UploadedInfo
-                  uploadResponse={uploadResponse}
-                  onRevoke={onRevoke}
-                  revokeOnlyThisCredential
-                />
-              }
+              <div className="title">
+                {
+                    showContainer &&
+                    <UploadedInfo
+                        uploadResponse={uploadResponse}
+                        onRevoke={onRevoke}
+                        revokeOnlyThisCredential
+                    />
+                }
 
-              <MessageLoader loading={uploading} message={'Subiendo archivo...'} />
+                <MessageLoader loading={uploading} message={'Subiendo archivo...'} />
 
-              {validation && renderValidation()}
+                {validation && renderValidation()}
 
-              <div className="buttonSection">
-                {renderCreateButton()}
+                <div className="buttonSection">
+                  {renderCreateButton()}
 
-                {!uploading && !uploadResponse && (
-                  <>
-                    {source.showValidate && (
-                      <ButtonPrimary
-                        text="Validar"
-                        theme="ThemePrimary"
-                        onClick={handleValidate}
-                        disabled={!showContainer}
-                      />
-                    )}
-                    {showContainer && (
-                      <ButtonPrimary
-                        text={buttonText ?? 'Crear credenciales'}
-                        theme="ThemePrimary"
-                        onClick={() => handleUpload()}
-                        disabled={!showContainer}
-                      />
-                    )}
-                  </>
-                )}
+                  {!uploading && !uploadResponse && (
+                      <>
+                        {source.showValidate && (
+                            <ButtonPrimary
+                                text="Validar"
+                                theme="ThemePrimary"
+                                onClick={handleValidate}
+                                disabled={!showContainer}
+                            />
+                        )}
+                        {showContainer && (
+                            <ButtonPrimary
+                                text={buttonText ?? 'Crear credenciales'}
+                                theme="ThemePrimary"
+                                onClick={() => handleUpload()}
+                                disabled={!showContainer}
+                            />
+                        )}
+                      </>
+                  )}
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
